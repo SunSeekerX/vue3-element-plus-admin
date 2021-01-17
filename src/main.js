bdc0c114da4b4be3c6ecabcef5d05777f989c618
SunSeekerX <1647800606@qq.com>
2021-01-17 18:37:16 +0800

    feat: init

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+/**
+ * @name:
+ * @author: SunSeekerX
+ * @Date: 2021-01-17 18:27:53
+ * @LastEditors: SunSeekerX
+ * @LastEditTime: 2021-01-17 18:35:20
+ */
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+createApp(App).use(store).use(router).mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,15 @@
-import { createStore } from "vuex";
+/**
+ * @name:
+ * @author: SunSeekerX
+ * @Date: 2021-01-17 18:27:53
+ * @LastEditors: SunSeekerX
+ * @LastEditTime: 2021-01-17 18:35:04
+ */
+import { createStore } from 'vuex'
 
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
-});
+  modules: {},
+})
